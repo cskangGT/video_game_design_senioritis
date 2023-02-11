93e41a3df3f09043b257614667b21d924c2d33d8
Won Yang <wyang342@gatech.edu>
2023-02-11 12:49:17 -0500

    add player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,30 +3,21 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    
-    private Rigidbody rb;
     private float movementX;
     private float movementY;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
 
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        movementX = movementVector.x / 10;
+        movementY = movementVector.y / 10;
     }
 
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
-        rb.AddForce(movement * speed);
+        transform.Translate(movement);
     }
 }
--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 ## To dos
 
-1. Create 2 spheres
-2. Create 2 rooms with 2 doors
+2. Create 2 doors
 3. Create 2 buttons that the spheres can roll onto
-4. Code player movement
-5. Code toggling functionality
-6. Code the doors so that they disappear when both buttons are pressed
+4. Code toggling functionality
+5. Code the doors so that they disappear when both buttons are pressed
