6518cabc3a107f9b453d108c717f5acafbea5920
Won Yang <wyang342@gatech.edu>
2023-02-11 12:41:48 -0500

    add camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float smoothness;
+    public Transform player;
+    private Vector3 offset;
+    private Vector3 cameraPosition;
+
+    void Start()
+    {  
+        offset = new Vector3(0, 20f, -5f);
+    }
+
+    void LateUpdate()
+    {
+        cameraPosition = player.position + offset;
+        transform.position = Vector3.Lerp(transform.position, cameraPosition, smoothness*Time.fixedDeltaTime);
+    }
+}
