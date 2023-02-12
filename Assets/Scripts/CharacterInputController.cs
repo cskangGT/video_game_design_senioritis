9801a2f4662078c19a6726c5035b5c3f30373754
Andrew Mo <andrewmo15@Andrews-MacBook-Pro-2.local>
2023-02-12 13:01:51 -0500

    player movement fixed

--- Assets/Scripts/CharacterInputController.cs
+++ Assets/Scripts/CharacterInputController.cs
@@ -1,29 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CharacterInputController : MonoBehaviour
 {
+    public float speed = 0;
+
+    private Rigidbody rb;
+
     private float movementX;
     private float movementY;
-    public bool enabled;
-    
-    void OnMove(InputValue movementValue)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
 
-        movementX = movementVector.x / 10;
-        movementY = movementVector.y / 10;
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 
     private void FixedUpdate()
     {
-        if (!enabled)
-        {
-            return;
-        }
-        
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
-        transform.Translate(movement);
+        rb.AddForce(movement * speed);
     }
 }
